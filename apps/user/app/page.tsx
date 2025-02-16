19be393a2f56a567edbc0096b94890ce68bd19a9
jaibhagtani <jaibhagtani2@gmail.com>
2025-02-16 23:02:41 +0530

    no change

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import { NEXT_AUTH } from "./lib/auth";
 
 
+
+
 export default async function Page() {
 
   const session = await getServerSession(NEXT_AUTH);

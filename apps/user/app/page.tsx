842eace38c3c8f7ea27dae97f4e790e7e797fa68
jaibhagtani <jaibhagtani2@gmail.com>
2025-02-17 19:47:01 +0530

    removed spaces

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -3,9 +3,6 @@ import { redirect } from "next/navigation";
 import { NEXT_AUTH } from "./lib/auth";
 
 
-
-
-
 export default async function Page() {
 
   const session = await getServerSession(NEXT_AUTH);

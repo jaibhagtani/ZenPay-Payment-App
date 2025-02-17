089c4d663d3cee0ca5ca041e6e356e9f5f7572f1
jaibhagtani <162352955+jaibhagtani@users.noreply.github.com>
2025-02-17 19:52:19 +0530

    Revert "Jai"

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -2,7 +2,6 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { NEXT_AUTH } from "./lib/auth";
 
-
 export default async function Page() {
 
   const session = await getServerSession(NEXT_AUTH);

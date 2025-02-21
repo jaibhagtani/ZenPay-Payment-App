42ea508a1445ac356244034ddc5b59c50e333ab9
jaibhagtani <jaibhagtani2@gmail.com>
2025-02-22 01:36:57 +0530

    changes during pro

--- apps/user/app/layout.tsx
+++ apps/user/app/layout.tsx
@@ -1,10 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { AppBarClient } from "../components/appbarclient";
 import Providers from "../providers";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ZenPay App",
@@ -21,7 +19,7 @@ export default function RootLayout({
   return (
       <html lang="en">
         <Providers>
-          <body className={inter.className}>
+          <body>
               <div className="min-w-screen bg-pink-50">
                 <AppBarClient />
                 {children}

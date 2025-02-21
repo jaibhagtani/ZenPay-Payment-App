c47fa945317745e1fe97543ae927d958e452deab
jaibhagtani <jaibhagtani2@gmail.com>
2025-02-21 16:16:11 +0530

    changed during pro

--- apps/user/app/lib/auth.ts
+++ apps/user/app/lib/auth.ts
@@ -74,7 +74,7 @@ export const NEXT_AUTH = {
         },
       })
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET || "SecRet",
   callbacks: {
     // fix the type here
     async session({ token, session }: any) {

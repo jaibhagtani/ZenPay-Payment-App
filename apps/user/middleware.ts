55f24a3989b480c01854c0b984b7deea7ddb05e6
jaibhagtani <jaibhagtani2@gmail.com>
2025-03-26 19:09:48 +0530

    updated middleware

--- apps/user/middleware.ts
+++ apps/user/middleware.ts
@@ -4,7 +4,8 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   if(request.nextUrl.pathname.startsWith('/dashboard') || request.nextUrl.pathname.startsWith('/transfer') ||
   request.nextUrl.pathname.startsWith('/trasactions') || request.nextUrl.pathname.startsWith('/p2p') || 
-  request.nextUrl.pathname.startsWith('/mpin') || request.nextUrl.pathname.startsWith('/api/mpin'))
+  request.nextUrl.pathname.startsWith('/mpin/set') || request.nextUrl.pathname.startsWith('/api/mpin') || 
+  request.nextUrl.pathname.startsWith('/balance') || request.nextUrl.pathname.startsWith('/profile'))
   {
     const token = request.cookies.get('next-auth.session-token')?.value
     if (!token) {

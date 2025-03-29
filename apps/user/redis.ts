87f38f6e2557a287f55f182e342db0f19310f164
jaibhagtani <jaibhagtani2@gmail.com>
2025-03-30 02:09:30 +0530

    issue resolving

--- apps/user/redis.ts
+++ apps/user/redis.ts
@@ -3,7 +3,7 @@ import { createClient } from "redis"
 const redisUrl = process.env.REDIS_URL;
 
 export const redisclient = createClient ({
-  url : redisUrl
+  url : `${process.env.REDIS_URL}'
 });
 
 

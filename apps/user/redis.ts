2bf7b46f5a352fa59ac84500953695627540b3f5
jaibhagtani <jaibhagtani2@gmail.com>
2025-03-30 01:37:20 +0530

    redis Error

--- apps/user/redis.ts
+++ apps/user/redis.ts
@@ -6,9 +6,9 @@ export const redisclient = createClient ({
   url : redisUrl
 });
 
-await redisclient.connect()
 
 redisclient.on("error", function(err :any) {
   throw err;
 });
 
+await redisclient.connect()

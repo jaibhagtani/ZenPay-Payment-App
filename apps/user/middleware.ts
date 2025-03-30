e5debc03581e63a4ec757988d63e3c84324dc615
jaibhagtani <jaibhagtani2@gmail.com>
2025-03-30 17:12:51 +0530

    issue resolving

--- apps/user/middleware.ts
+++ apps/user/middleware.ts
@@ -13,6 +13,7 @@ export function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL('/auth/signin', request.url));
     }
     return NextResponse.next();
+    
   }
   
   return NextResponse.next();

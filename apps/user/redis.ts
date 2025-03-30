0987b2095600293639bce77c9faf8d8467981182
jaibhagtani <jaibhagtani2@gmail.com>
2025-03-30 18:16:47 +0530

    issue resolving

--- apps/user/redis.ts
+++ apps/user/redis.ts
@@ -5,10 +5,10 @@ console.log("REDIS_URL =", process.env.REDIS_URL);
 if (!redisUrl) {
   throw new Error("REDIS_URL is not defined");
 }
+
 export const redisclient = createClient({
   url: redisUrl,
 });
-
 
 redisclient.on("error", (err) => {
   console.error("Redis error:", err);

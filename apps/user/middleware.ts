bfe0aa13d0825182f31c75e79cad2d6adae180a9
jaibhagtani <jaibhagtani2@gmail.com>
2025-04-01 13:24:00 +0530

    resolved signUp

--- apps/user/middleware.ts
+++ apps/user/middleware.ts
@@ -2,8 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  if(request.nextUrl.pathname.startsWith('/dashboard') || request.nextUrl.pathname.startsWith('/transfer') ||
-  request.nextUrl.pathname.startsWith('/trasactions') || request.nextUrl.pathname.startsWith('/p2p') || 
+  if(request.nextUrl.pathname.startsWith('/transfer') || request.nextUrl.pathname.startsWith('/trasactions') || request.nextUrl.pathname.startsWith('/p2p') || 
   request.nextUrl.pathname.startsWith('/mpin') || request.nextUrl.pathname.startsWith('/api/mpin') || 
   request.nextUrl.pathname.startsWith('/balance') || request.nextUrl.pathname.startsWith('/profile') )
   {

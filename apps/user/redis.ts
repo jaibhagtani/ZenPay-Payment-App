3da54a999c33f5088fb101414658ada65c504f87
jaibhagtani <jaibhagtani2@gmail.com>
2025-06-24 18:55:35 +0530

    Dashboard Improved RU

--- apps/user/redis.ts
+++ apps/user/redis.ts
@@ -3,7 +3,7 @@ import { createClient } from "redis";
 
 dotenv.config();
 
-const redisUrl = process.env.REDIS_URL;
+const redisUrl = "rediss://default:AaJnAAIjcDE0OWIxZTg2YmJkYWY0MzRmOTBmMzA0ZWU3ZjhmNzY2N3AxMA@trusty-lemur-41575.upstash.io:6379";
 if (!redisUrl) {
   throw new Error("REDIS_URL is not defined in process.env");
 }

ce9dd25dce173e70975fc3b7343a56b5c5f724c1
jaibhagtani <jaibhagtani2@gmail.com>
2025-07-01 04:20:51 +0530

    updated middleware

--- apps/user/middleware.ts
+++ apps/user/middleware.ts
@@ -2,9 +2,11 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  if(request.nextUrl.pathname.startsWith('/dashboard') || request.nextUrl.pathname.startsWith('/transfer') || request.nextUrl.pathname.startsWith('/trasactions') || request.nextUrl.pathname.startsWith('/p2p') || 
+  if(request.nextUrl.pathname.startsWith('/dashboard') || request.nextUrl.pathname.startsWith('/transfer') || 
+  request.nextUrl.pathname.startsWith('/trasactions') || request.nextUrl.pathname.startsWith('/p2p') || 
   request.nextUrl.pathname.startsWith('/mpin') || request.nextUrl.pathname.startsWith('/api/mpin') || 
-  request.nextUrl.pathname.startsWith('/balance') || request.nextUrl.pathname.startsWith('/profile') )
+  request.nextUrl.pathname.startsWith('/balance') || request.nextUrl.pathname.startsWith('/profile') || 
+  request.nextUrl.pathname.startsWith('/split-bill') || request.nextUrl.pathname.startsWith('/notificationspendings'))
   {
     const token = (request.cookies.get('next-auth.session-token')?.value) || (request.cookies.get('__Secure-next-auth.session-token')?.value)
     // alert("User not Logged in!!")
